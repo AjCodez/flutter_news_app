ea79e4208c0ac945fe7d05f688cee028a0865521
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-11 21:58:34 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,27 @@ import 'components/customListTile.dart';
 import 'model/article_model.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      routes: {
+        '/news/': (context) => NewsBuilder(),
+      },
     );
   }
 }
 
 class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -31,39 +39,97 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("News App", style: TextStyle(color: Colors.black)),
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.green,
         iconTheme: const IconThemeData(color: Colors.black),
       ),
-      body: NewsBuilder(
-        category: 'general',
+      body: FutureBuilder(
+        future: client.getArticle('general'),
+        builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
+          //let's check if we got a response or not
+          if (snapshot.hasData) {
+            //Now let's make a list of articles
+            List<Article> articles = snapshot.data;
+            return ListView.builder(
+              //Now let's create our custom List tile
+              itemCount: articles.length,
+              itemBuilder: (context, index) =>
+                  customListTile(articles[index], context),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
-
-      //Now let's call the APi services with futurebuilder wiget
-      // body: FutureBuilder(
-      //   future: client.getArticle('sports'),
-      //   builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
-      //     //let's check if we got a response or not
-      //     if (snapshot.hasData) {
-      //       //Now let's make a list of articles
-      //       List<Article> articles = snapshot.data;
-      //       return ListView.builder(
-      //         //Now let's create our custom List tile
-      //         itemCount: articles.length,
-      //         itemBuilder: (context, index) =>
-      //             customListTile(articles[index], context),
-      //       );
-      //     }
-      //     return const Center(
-      //       child: CircularProgressIndicator(),
-      //     );
-      //   },
-      // ),
       drawer: Drawer(
         child: ListView(
-          children: const [
+          children: [
             ListTile(
-              title: Text('Hello'),
-            )
+              title: const Text('Business'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return NewsBuilder(
+                    category: 'business',
+                  );
+                }));
+              },
+            ),
+            ListTile(
+              title: const Text('Entertainment'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return NewsBuilder(
+                    category: 'entertainment',
+                  );
+                }));
+              },
+            ),
+            ListTile(
+              title: const Text('Sports'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return NewsBuilder(
+                    category: 'sports',
+                  );
+                }));
+              },
+            ),
+            ListTile(
+              title: const Text('Health'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return NewsBuilder(
+                    category: 'health',
+                  );
+                }));
+              },
+            ),
+            ListTile(
+              title: const Text('Science'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return NewsBuilder(
+                    category: 'science',
+                  );
+                }));
+              },
+            ),
+            ListTile(
+              title: const Text('Technology'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return NewsBuilder(
+                    category: 'technology',
+                  );
+                }));
+              },
+            ),
           ],
         ),
       ),
--- lib/pages/NewsPage.dart
+++ lib/pages/NewsPage.dart
@@ -11,24 +11,33 @@ class NewsBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: client.getArticle(category),
-      builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
-        //let's check if we got a response or not
-        if (snapshot.hasData) {
-          //Now let's make a list of articles
-          List<Article> articles = snapshot.data;
-          return ListView.builder(
-            //Now let's create our custom List tile
-            itemCount: articles.length,
-            itemBuilder: (context, index) =>
-                customListTile(articles[index], context),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Top ${category.replaceFirst(category[0], category[0].toUpperCase())} Headlines',
+          style: const TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: FutureBuilder(
+        future: client.getArticle(category),
+        builder: (BuildContext context, AsyncSnapshot<List<Article>> snapshot) {
+          //let's check if we got a response or not
+          if (snapshot.hasData) {
+            //Now let's make a list of articles
+            List<Article> articles = snapshot.data;
+            return ListView.builder(
+              //Now let's create our custom List tile
+              itemCount: articles.length,
+              itemBuilder: (context, index) =>
+                  customListTile(articles[index], context),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
           );
-        }
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
+        },
+      ),
     );
   }
 }
